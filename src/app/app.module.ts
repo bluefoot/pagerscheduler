c66e9386dfa703b42cb6a21fda953ceaf6ff2b95
Gewton Teixeira <gewtonj@br.ibm.com>
2018-05-26 15:12:57 -0300

    Basic styles and components structure, also bumping to angular 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
+// Core stuff
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+// Material stuff
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatDatepickerModule, MatNativeDateModule,
+  MatInputModule,MatFormFieldModule } from '@angular/material';
+
+// App stuff
+import { AppRoutes } from './routes';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ScheduleFormComponent } from './schedule-form/schedule-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ScheduleFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes),
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
